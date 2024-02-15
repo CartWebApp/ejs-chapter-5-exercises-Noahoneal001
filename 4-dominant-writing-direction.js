19bbf429ab0c46566726fd1afb12234068f8436b
Noahoneal001 <143848565+Noahoneal001@users.noreply.github.com>
2024-02-15 15:22:34 -0800

    Done

--- 4-dominant-writing-direction.js
+++ 4-dominant-writing-direction.js
@@ -1,21 +1,43 @@
 const SCRIPTS = require('./includes/scripts.js');
 
 
+
 // require('./includes/scripts.js') // include the scripts used by the chapter
 
 function dominantDirection(text) {
     // Your code here.
-    let charCodeList = []
-    let directionList = []
+    let output = ``;
+    let charCodeList = [];
+    let directionList = [];
+    let tally = {ltr: 0, rtl:0, ttb:0};
     for(let i = 0; i < text.length; i++) {
-      charCodeList.push(text.codePointAt(i))
-    }
+      charCodeList.push(text.codePointAt(i));
+    };
 
-    for(const script of SCRIPTS) {
-      if(script.ranges.some(([from,to]) => {return code >= from && code < to})) {
-        directionList.push(script.direction)
-      }
-    }
+    charCodeList.forEach((code) => {
+      for (let script of SCRIPTS) {
+        if (script.ranges.some(([from, to]) => {
+          return code >= from && code < to;
+        })) {
+          directionList.push(script.direction);
+        };
+      };
+    });
+
+    directionList.forEach((tester) => {
+      if(tester == `ltr`) {tally.ltr++}
+      else if (tester == `rtl`) {tally.rtl++}
+      else if (tester == `ttb`) {tally.ttb++}
+    });
+
+    if (tally.ltr > tally.rtl && tally.ltr > tally.ttb) {return `ltr`}
+    else if (tally.rtl > tally.ltr && tally.rtl > tally.ttb) {return `rtl`}
+    else if (tally.ttb > tally.ltr && tally.ttb > tally.rtl) {return `ttb`}
+    else if (tally.rtl == tally.ltr && tally.rtl == tally.ttb) {return `Equal`}
+    else if (tally.rtl == tally.ltr) {return `rtl and ltr`}
+    else if (tally.rtl == tally.ttb) {return `rtl and ttb`}
+    else if (tally.ttb == tally.ltr) {return `ttb and ltr`}
+
   }
   
   console.log(dominantDirection("Hello!"));
